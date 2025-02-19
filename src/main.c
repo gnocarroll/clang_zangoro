aa8aab354fb5233ed32d41c5361ae15720a35bd0
gnocarroll <gocarrol@purdue.edu>
2025-02-18 20:12:30 -0500

    basic startup done

--- src/main.c
+++ src/main.c
@@ -1,28 +1,64 @@
-#include <stdio.h>
+#include <stdlib.h>
 
-#include "bitset.h"
+#define SDL_MAIN_USE_CALLBACKS
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
-#define N_BITS (10)
+#define TITLE "ZANGORO"
 
-int main() {
-    char bitset[BITSET_SIZE(N_BITS)];
-    char str[50] = { 0 };
+#define WINDOW_W  (800)
+#define WINDOW_H  (600)
+#define WINDOW_FL (0)
 
-    printf("%s\n", bitset_str(str, bitset, N_BITS));
+#define INIT_FLAGS (SDL_INIT_AUDIO | SDL_INIT_VIDEO | SDL_INIT_JOYSTICK | \
+                    SDL_INIT_HAPTIC | SDL_INIT_GAMEPAD | SDL_INIT_EVENTS)
 
-    for (int i = 0; i < N_BITS; i += 2) {
-        bitset_set(bitset, i, true);
+SDL_Window   *window;
+SDL_Renderer *render;
+
+void destroy_sdl(void);
+
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
+    (void) appstate;
+    (void) argc;
+    (void) argv;
+
+    // initialize global state and SDL
+
+    if (!SDL_Init(INIT_FLAGS) ||
+        !(window = SDL_CreateWindow(TITLE, WINDOW_W, WINDOW_H, WINDOW_FL)) ||
+        !(render = SDL_CreateRenderer(window, NULL))) {
+        SDL_LogCritical(
+            SDL_LOG_CATEGORY_ERROR, 
+            "critical failure during startup, exiting\n"
+        );
+        return SDL_APP_FAILURE;
     }
-    
-    printf("%s\n", bitset_str(str, bitset, N_BITS));
 
-    printf("printing bits one by one...\n");
+    // cleanup window, renderer, and subsystems at exit
 
-    for (int i = 0; i < N_BITS; i++) {
-        printf("%d", bitset_get(bitset, i));
-    }
+    atexit(destroy_sdl);
+}
 
-    printf("\n");
+void destroy_sdl(void) {
+    SDL_DestroyRenderer(render);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
 
-    return 0;
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
+    (void) appstate;
+
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void *appstate) {
+    (void) appstate;
+
+    return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void *appstate, SDL_AppResult result) {
+    (void) appstate;
+    (void) result;
 }
