519bcae165028d9ce349117a3f4d8cd6c8eabd98
gnocarroll <gocarrol@purdue.edu>
2025-02-21 15:00:22 -0500

    create mostly empty files for emscripten client networking and develop ring buffer some

--- include/ring_buffer.h
+++ include/ring_buffer.h
@@ -1,15 +1,23 @@
 #ifndef RING_BUFFER_H
 #define RING_BUFFER_H
 
+#include <stdbool.h>
+#include <stddef.h>
+
+// to use define your own struct which contains this as its first member
+// then set the size properly and you can cast a pointer to your struct to a
+// ring_buffer * to use these functions with it
+
 typedef struct ring_buffer {
-    size_t size;
-    size_t begin;
-    size_t end;
-    char[] data;
+    size_t size;   // buffer capacity
+    size_t begin;  // first occupied byte and where data is consumed from
+    size_t end;    // one after last occupied, data is added here
+    bool is_empty; // take a wild guess...
+    char[] data;   // in your struct there would be buffer after
 } ring_buffer;
 
 void ring_buffer_init(ring_buffer* rb);
-void ring_buffer_append(ring_buffer *rb, void *src, size_t count);
-void ring_buffer_consume(ring_buffer *rb, void *dst, size_t count);
+ssize_t ring_buffer_append(ring_buffer *rb, void *src, size_t count);
+ssize_t ring_buffer_consume(ring_buffer *rb, void *dst, size_t count);
 
 #endif // RING_BUFFER_H
---
+++ src/networking/emscripten_client.c
@@ -0,0 +1,9 @@
+#include "networking/emscripten_client.h"
+
+#ifdef __EMSCRIPTEN__
+
+#include <emscripten/websocket.h>
+
+
+
+#endif // __EMSCRIPTEN__
